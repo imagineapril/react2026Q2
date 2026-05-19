36e3287274095b4c584ef8651ac205655299be38
Iuliia <64987202+imagineapril@users.noreply.github.com>
2026-05-19 16:35:07 +0300

    refactor: change class component in ErrorTestButton.tsx into functional component

--- src/components/Button/ErrorTestButton.tsx
+++ src/components/Button/ErrorTestButton.tsx
@@ -1,33 +1,22 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import styles from './Button.module.css';
-import type { ErrorTestButtonProps, ErrorTestButtonState } from '../../types';
 
+const ErrorTestButton = () => {
+  const [shouldThrow, setShouldThrow] = useState(false);
 
-class ErrorTestButton extends Component<ErrorTestButtonProps, ErrorTestButtonState> {
-  constructor(props: ErrorTestButtonProps) {
-    super(props);
-    this.state = {
-      shouldThrow: false,
-    };
-  }
-
-  handleThrowError = () => {
-    this.setState({ shouldThrow: true }, () => {
-      throw new Error('Test error triggered by user! This is a simulated error for testing Error Boundary.');
-    });
-  };
-
-  render() {
-    if (this.state.shouldThrow) {
+  if (shouldThrow) {
       throw new Error('Test error');
     }
 
-    return (
-      <button className={styles.button} onClick={this.handleThrowError}>
-        Test Error Boundary
-      </button>
-    );
-  }
+  const handleThrowError = () => {
+    setShouldThrow(true);
+  };
+
+  return (
+    <button className={styles.button} onClick={handleThrowError}>
+      Test Error Boundary
+    </button>
+  );
 }
 
 export default ErrorTestButton;
