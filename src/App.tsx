0d7bbe0d9b67fa238ad5cc5b97bd5b0a21462b83
Julia Timokhina <juliatimokhina@Julias-MacBook-Pro.local>
2026-06-06 21:47:45 +0300

    chore: setup react vite project with ESLint, Prettier and Husky

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>React Forms Task</h1>
+      <p>Здесь будут формы и модальные окна</p>
+    </div>
+  );
+}
+
+export default App;
